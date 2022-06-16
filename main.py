268559a86f87b0c7c3788c3dd9844775b952647f
beom-yun <scds0326@naver.com>
2022-06-16 19:31:38 +0900

    check_url() part One

--- main.py
+++ main.py
@@ -2,27 +2,29 @@ import os
 import requests
 
 
+def check_url(url):
+    pass
+
+
+def is_it_right_url(url):
+    pass
+
+
 def should_i_retry():
     while True:
         try:
             print('Do you want to start over? y/n', end=' ')
-            input_data = input().strip().lower()
-            return {'y': True, 'n': False}[input_data]
-
-            # if input_data == 'y':
-            #     return True
-            # elif input_data == 'n':
-            #     return False
-            # else:
-            #     raise Exception()
+            return {'y': True, 'n': False}[input().strip().lower()]
         except:
             print("That's not a valid answer.")
 
 
-should_i_retry()
+while True:
+    print('Welcome to IsItDown.py!')
+    print('Please write a URL or URLs you want to check. (saperated by comma)')
 
-# while True:
-#     print('Welcome to IsItDown.py!')
-#     print('Please write a URL or URLs you want to check. (saperated by comma)')
-#     input_data = input()
-#     print('input_data', input_data)
+    for input_url in [x.strip().lower() for x in input().split(',')]:
+        check_url(input_url)
+
+    if not should_i_retry():
+        break
